6e9aaadfd4967f9392dbf198ce3152fc437a1577
Peter Thai <petertie@cs.utexas.edu>
2017-10-25 21:13:05 -0500

    all item names fixed of duplicate names

--- app/AutobotsRollOut/filerenamerparenremover.py
+++ app/AutobotsRollOut/filerenamerparenremover.py
@@ -5,7 +5,23 @@ paths = (os.path.join(root, filename)
         for filename in filenames)
 
 for path in paths:
-    # the '#' in the example below will be replaced by the '-' in the filenames in the directory
-    newname = path.replace('(', '#')
-    if newname != path:
-        os.rename(path, newname)
+    print(path)
+    splitpath = path.split("/")
+    print(splitpath)
+    oldfilename = splitpath[len(splitpath)-1]
+    print(oldfilename)
+    #time to remove paren number paren
+    oldfilenamesplit = oldfilename.split(" ")
+    print(oldfilenamesplit)
+    #check if need correction
+    if ")" in oldfilenamesplit[len(oldfilenamesplit)-1]:
+        oldfilenamesplit[len(oldfilenamesplit)-1] = ".png"
+        newfilename = " ".join(oldfilenamesplit)
+        print(newfilename)
+        #remove 1 extra space from end of filename
+        newfilename = newfilename.rsplit(" ", 1)
+        newfilename = "".join(newfilename)
+        print(newfilename)
+        newpath = path.replace(oldfilename, newfilename)
+        print(newpath)
+        os.rename(path, newpath)
